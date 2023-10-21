a6fac64b3a0d5decbd2ec3487f44286eb8890b0d
XINGMENGd <lsw143722@gmail.com>
2023-10-21 13:59:30 +0800

    Updates

---
+++ assets/index-6ba9b281.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as n}from"./index-010153ca.js";const o={};function r(t,s){return c(),n("div",null," menu1-1 ")}const a=e(o,[["render",r]]);export{a as default};
---
+++ assets/index-a709aaba.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as s,f as c}from"./index-010153ca.js";const o={},n=c("div",{class:"test"}," 我是vue3.2+vite2+vue-router4 ",-1),r=[n];function _(a,d){return t(),s("div",null,r)}const f=e(o,[["render",_]]);export{f as default};
